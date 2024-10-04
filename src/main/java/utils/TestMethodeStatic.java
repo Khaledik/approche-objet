342fc32141f8ad63e798d30a3bba73b19ca6e558
Khaledik <khaled.ikhlef2014@gmail.com>
2024-10-04 16:58:47 +0200

    tp_04

---
+++ src/main/java/utils/TestMethodeStatic.java
@@ -0,0 +1,18 @@
+package utils;
+
+public class TestMethodeStatic {
+    public static void main(String[] args) {
+
+        String myString = "16";
+        int myInt = Integer.parseInt(myString);
+
+        int a = 27;
+        int b = 5;
+
+
+        System.out.println(Integer.max(a, b));
+
+
+
+    }
+}
