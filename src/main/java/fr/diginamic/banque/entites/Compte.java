3e42ecfc1cc32c4d8c676f50b6bfffc0c2e1839c
Khaledik <khaled.ikhlef2014@gmail.com>
2024-10-07 11:03:45 +0200

    tp_06

--- src/main/java/fr/diginamic/banque/entites/Compte.java
+++ src/main/java/fr/diginamic/banque/entites/Compte.java
@@ -2,11 +2,17 @@ package fr.diginamic.banque.entites;
 
 public class Compte {
 
-    int numeroDeCompte;
-    double soldeDuCompte;
+    static int numeroDeCompte;
+    static double soldeDuCompte;
 
-    public Compte (int numeroDeCompte, double soldeDuCompte) {
+    public Compte(int numeroDeCompte, double soldeDuCompte) {
         this.numeroDeCompte = numeroDeCompte;
         this.soldeDuCompte = soldeDuCompte;
     }
+
+    public  String toString() {
+        return "Compte numéro : " + numeroDeCompte + ", Solde : " + soldeDuCompte + " €";
+    }
+
+
 }
--- src/main/java/fr/diginamic/banque/entites/TestBanque.java
+++ src/main/java/fr/diginamic/banque/entites/TestBanque.java
@@ -6,7 +6,7 @@ public class TestBanque {
 
         Compte monCompte = new Compte(145482521, 400.94);
 
-        System.out.println(monCompte);
+        System.out.println(monCompte.toString());
 //      Je constate que cela ne m'affiche pas le contenu de mon instance monCompte
     }
 }
